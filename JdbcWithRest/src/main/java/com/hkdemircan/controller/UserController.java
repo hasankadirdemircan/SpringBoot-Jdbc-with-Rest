104ae69d004487732b52a568fa310ed19d1fb5ef
hkdemircan <hkdemircan06@gmail.com>
2019-03-05 23:35:25 +0300

    update and createUser added in Controller and update controller beans

--- JdbcWithRest/src/main/java/com/hkdemircan/controller/UserController.java
+++ JdbcWithRest/src/main/java/com/hkdemircan/controller/UserController.java
@@ -6,11 +6,11 @@ import com.hkdemircan.model.User;
 import com.hkdemircan.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -26,7 +26,7 @@ public class UserController {
     }
 
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/user/{id}")
+    @DeleteMapping("/user/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable("id") Long id){
         try{
             userService.deleteUser(id);
@@ -38,10 +38,36 @@ public class UserController {
         }
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/users")
+    @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers(){
         List<User> users = userService.findUsers();
        return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable("id") Long id, @RequestBody User userRequest){
+        try{
+            User user = userService.findUser(id);
+            user.setFirstName(userRequest.getFirstName());
+            user.setLastName(userRequest.getLastName());
+            userService.updateUser(user);
+        return ResponseEntity.ok().build();
+        }catch (UserNotFoundException ex){
+            return  ResponseEntity.notFound().build();
+        }catch (Exception ex){
+            throw new InternalServerException(ex);
+        }
+    }
+    @PostMapping("/user")
+    public ResponseEntity<URI> createUser(@RequestBody User user){
+        try{
+            userService.createUser(user);
+            Long id = user.getId();
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
+            return ResponseEntity.created(location).build();
+        }catch (Exception ex){
+            throw new InternalServerException(ex);
+        }
     }
 
 }
